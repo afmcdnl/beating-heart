a4c2f2fb0bdd0e9ffceac8bc9dc639e43de51425
uottawamakermobile <74430656+uottawamakermobile@users.noreply.github.com>
2020-11-13 15:42:07 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+})
 for (let index = 0; index < 4; index++) {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
