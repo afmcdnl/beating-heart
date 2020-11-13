04d90ce8a1d0ec1ea7b703fe33597b7a26acefc5
Andrew <afmcdnl@gmail.com>
2020-11-13 15:28:48 -0500

    Added a loop to repeat 4 times

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-basic.showIcon(IconNames.Heart)
-basic.pause(100)
-basic.showIcon(IconNames.SmallHeart)
-basic.pause(100)
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+}
 basic.forever(function () {
 	
 })
